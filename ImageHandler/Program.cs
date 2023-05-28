af268eff6bef381029832a08a6d654ea95dfff04
Cyrus <sgktost1@gmail.com>
2023-05-28 18:57:34 +0500

    Init

---
+++ ImageHandler/Program.cs
@@ -0,0 +1,11 @@
+namespace ImageHandler;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MyForm());
+    }
+}
